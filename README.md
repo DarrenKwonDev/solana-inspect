6b57d713bdfb86d9c683ebec8d3302fcdca27967
drkup <darrenkwondev46@gmail.com>
2025-12-20 19:30:02 +0900

    chore: README

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ cli for inspect solana blockchain
 ## tools 
 - [ ] rpc_check   
 - [ ] dex_count   
-- [ ] swap_watch (jupyter, raydium, orca)
+- [ ] swap_watch
+  - swap pool을 소유한 곳만 대상 (raydium, meteora, orca)
+  - aggregator(jupyter, okx dex)와 aggregator를 대상으로 한 private pool(solfi) 등은 제외 
--- src/bin/swap_watch.rs
+++ src/bin/swap_watch.rs
@@ -1,7 +1,11 @@
 use std::env;
 
 use anyhow::Result;
-use solana_client::nonblocking::pubsub_client::PubsubClient;
+use solana_client::{
+    nonblocking::pubsub_client::PubsubClient,
+    rpc_config::{RpcBlockSubscribeConfig, RpcBlockSubscribeFilter},
+};
+use solana_sdk::commitment_config::CommitmentConfig;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -12,7 +16,28 @@ async fn main() -> Result<()> {
 
     let pubsub = PubsubClient::new(&wss_url).await?;
 
-    println!("{:?}", pubsub);
+    // 모든 tx를 분석한다는 목표라면
+    // slot_subscribe + get_block 조합 사용하는 것이 좋음
+    // get_slot이 반환하는 slot은 최신이 아닐 수 있다. RPC 지연이 100ms를 초과하면 슬롯을 놓친다.
+    /*
+        pub async fn monitor_all_txs_subscribe(rpc: &RpcClient) {
+            let mut subscription = rpc.slot_subscribe()?;
+
+            while let Some(slot_update) = subscription.next().await {
+                let slot = slot_update.slot;
+
+                match rpc.get_block(slot) {
+                    Ok(block) => {
+                        process_all_txs(&block);  // 모든 TX 처리
+                    }
+                    Err(_) => {
+                        // 스킵된 슬롯 (네트워크 불안정)
+                        // → 다음 신호 기다림
+                    }
+                }
+            }
+        }
+    */
 
     Ok(())
 }
